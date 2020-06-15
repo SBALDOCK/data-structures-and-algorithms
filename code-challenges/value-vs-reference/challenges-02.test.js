8e51b713bf6d900273c927829b8c6c9beb837235
SBALDOCK <stephenbaldock78@gmail.com>
2020-06-15 12:13:20 -0700

    challenge 10 on time

--- code-challenges/value-vs-reference/challenges-02.test.js
+++ code-challenges/value-vs-reference/challenges-02.test.js
@@ -10,7 +10,7 @@ Write a function named raisedToTheThird that takes in an array of numbers and re
 
 const raisedToTheThird = (arr) => {
   let newArray = [];
-  arr.forEach(function(num) {
+  arr.forEach((num) => {
     const power = Math.pow(num, 3);
     newArray.push(power)
   });
@@ -82,9 +82,10 @@ console.log(people[1].isAuthor) prints true
 ------------------------------------------------------------------------------------------------ */
 
 const setStatusAsAuthor = (people) => {
-  // Solution code here...
-};
-
+  people.forEach((value) => { 
+  value.isAuthor = true;
+  });
+}
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6 - Stretch Goal
 
@@ -101,8 +102,7 @@ console.log(a) prints [1, 2, 3, 4]
 ------------------------------------------------------------------------------------------------ */
 
 const append = (arr1, arr2) => {
-  // Solution code here...
-  
+  arr1.push.apply(arr1, arr2);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -150,7 +150,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should add a property to every object in an array', () => {
     const a = [{ fullName: 'Octavia Butler' }, { fullName: 'Ray Bradbury' }, { fullName: 'Kurt Vonnegut' }];
     setStatusAsAuthor(a);
@@ -161,7 +161,7 @@ xdescribe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should append the second array to the first', () => {
     const a = [1, 2, 3, 4];
     const b = [5, 6, 7, 8];
