5d398dec41a5bf965cd8bfb19931de2a75675f75
SBALDOCK <stephenbaldock78@gmail.com>
2020-08-09 07:16:58 -0700

    combination

--- code-challenges/Chain/challenges-10.test.js
+++ code-challenges/Chain/challenges-10.test.js
@@ -147,7 +147,9 @@ let starWarsData = [{
 }];
 
 let findMaleAndFemale = (data) => {
-  // Solution code here...
+  data.filter((val, idx) => {
+    return 
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -244,14 +246,14 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should return only characters that are male or female', () => {
     expect(findMaleAndFemale(starWarsData)).toStrictEqual('Luke Skywalker and Darth Vader and Leia Organa');
     expect(findMaleAndFemale([{ name: 'person', gender: 'female' }, { gender: 'lol' }, { name: 'persontwo', gender: 'male' }])).toStrictEqual('person and persontwo');
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should return the name of the shortest character', () => {
     expect(findShortest(starWarsData)).toStrictEqual('R2-D2');
   });
