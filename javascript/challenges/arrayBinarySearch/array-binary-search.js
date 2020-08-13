531f29331f05b9fa69f110a7de837c16d82f1fa6
SBALDOCK <stephenbaldock78@gmail.com>
2020-08-13 08:05:21 -0700

    lab-3 correction

--- javascript/challenges/arrayBinarySearch/array-binary-search.js
+++ javascript/challenges/arrayBinarySearch/array-binary-search.js
@@ -6,7 +6,7 @@ function binarySearch(arr, kilmer){
   let firstPlace = 0;
   let lastPlace = arr.length-1;
   while(lastPlace>=firstPlace){
-     middle = Math.floor((firstPlace + lastPlace)/2);
+     let middle = Math.floor((firstPlace + lastPlace)/2);
     if(kilmer === arr[middle]){
       return middle;
     }
